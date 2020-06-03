34d78d834ad1781ad2a8af955a62d3808d7362e8
kushberrycream <tom@wilson-express.co.uk>
2020-06-03 16:22:17 +0000

    appended html differently in javascript

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,10 +1,8 @@
 // Variables
-const app = document.getElementById("data");
+const history = document.getElementById("data");
 const api = "https://api.spacexdata.com/v3/";
 
-
 callHistory()
-
 
 // calls spacex History api
 
@@ -13,29 +11,17 @@ function callHistory() {
     $("#data").addClass("container");
 
     axios.get(api + "history").then(response => {
+
         let data = response.data;
         let newData = data.slice().reverse();
-        let title = document.createElement("div");
-        title.setAttribute("class", "title");
-        app.appendChild(title);
-        title.innerHTML = `<h1 class="title">SpaceX Historical Events</h1>`;
-        let row = document.createElement("div");
+        let row1 = document.createElement("div");
 
-        row.setAttribute("class", "row no-gutters justify-content-center");
+        row1.setAttribute("class", "row no-gutters justify-content-center");
+               
         newData.forEach(item => {
             $("#loader").addClass("hide-loader");
 
-
-
-            let card = document.createElement("div");
-            card.setAttribute("class", "card col-lg-5");
-            app.appendChild(row);
-            row.appendChild(card);
-
-            let date = moment.parseZone(item.event_date_utc).utc().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
             // checks for a url within reddit item.
-
             let reddit = item.links.reddit;
 
             if (reddit == null) {
@@ -46,7 +32,6 @@ function callHistory() {
             }
 
             // checks for a url within article item.
-
             let article = item.links.article;
 
             if (article == null) {
@@ -56,21 +41,41 @@ function callHistory() {
                 article = `<a href="${item.links.article}" target="_blank"><i class="far fa-newspaper"></i></a>`;
             }
 
-            card.innerHTML = `
-                            <div class="card-header">
-                                <h3>${item.title}</h3>
-                            </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                    <h5 class="card-title">${date}</h5>
-                                    <p class="card-title">Flight No: ${item.flight_number} - ID: ${item.id} 
-                                        <span class="history-media-buttons"><a href="${item.links.wikipedia}" target="_blank"><i class="fab fa-wikipedia-w"></i></a> &nbsp; ${reddit} &nbsp; ${article}</span>
-                                    </p>
-                                    <p class="card-text">${item.details}</p>
-                                    </div>
-                                </div>
-                            </div>`;
+            let dateFormat = moment.parseZone(item.event_date_utc).utc().format("dddd, MMMM Do YYYY, h:mm:ss a");
+            let card = document.createElement("div");
+            let cardHead = document.createElement("div");
+            let title = document.createElement("h3");
+            let cardBody = document.createElement("div");
+            let row2 = document.createElement("div");
+            let col = document.createElement("div");
+            let date = document.createElement("h5");
+            let flight = document.createElement("p");
+            let details = document.createElement("p");
+
+            card.setAttribute("class", "card col-lg-5");
+            cardHead.setAttribute("class", "card-header");
+            cardBody.setAttribute("class", "card-body");
+            row2.setAttribute("class", "row");
+            col.setAttribute("class", "col");
+            date.setAttribute("class", "card-title");
+            flight.setAttribute("class", "card-title");
+            details.setAttribute("class", "card-text");
+
+            history.appendChild(row1);
+            row1.appendChild(card);
+            card.appendChild(cardHead);
+            cardHead.appendChild(title);
+            card.appendChild(cardBody);
+            cardBody.appendChild(row2);
+            row2.appendChild(col);
+            col.appendChild(date);
+            col.appendChild(flight);
+            col.appendChild(details);
+
+            title.innerText = item.title;
+            date.innerText = dateFormat;
+            flight.innerHTML = `Flight No: ${item.flight_number} - ID: ${item.id} <span class="history-media-buttons"><a href="${item.links.wikipedia}" target="_blank"><i class="fab fa-wikipedia-w"></i></a> &nbsp; ${reddit} &nbsp; ${article}</span>`;
+            details.innerText = item.details;
         });
     });
 }
